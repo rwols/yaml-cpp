d47bda8eb602d14d76764977ff1a3a3b9af7d2bd
Raoul Wols <raoulwols@gmail.com>
2017-02-02 17:56:20 +0100

    Give Emitter the ability to recursively emit STL containers.

--- include/yaml-cpp/stlemitter.h
+++ include/yaml-cpp/stlemitter.h
@@ -7,45 +7,163 @@
 #pragma once
 #endif
 
-#include <vector>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <forward_list>
 #include <list>
-#include <set>
 #include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <yaml-cpp/detail/has_left_shift.h>
 
 namespace YAML {
-template <typename Seq>
-inline Emitter& EmitSeq(Emitter& emitter, const Seq& seq) {
+
+// if the expression `emitter << myvalue;` is valid,
+// then we consider it a yaml scalar.
+template <class T>
+struct IsOutputScalar : public detail::has_left_shift<Emitter&, const T&> {};
+
+// nothing is a sequence by default.
+template <class T>
+struct IsOutputSequence : public std::false_type {};
+
+// nothing is a map by default.
+template <class T>
+struct IsOutputMap : public std::false_type {};
+
+// a primitive is a scalar or a sequence or a map.
+template <class T>
+struct IsOutputPrimitive {
+  static constexpr bool value = IsOutputScalar<T>::value ||
+                                IsOutputSequence<T>::value ||
+                                IsOutputMap<T>::value;
+};
+
+// std::vector
+// is an output sequence if and only if T is a primitive.
+template <class T, class Alloc>
+struct IsOutputSequence<std::vector<T, Alloc>> {
+  static constexpr bool value = IsOutputPrimitive<T>::value;
+};
+
+// std::list
+// is an output sequence if and only if T is a primitive.
+template <class T, class Alloc>
+struct IsOutputSequence<std::list<T, Alloc>> {
+  static constexpr bool value = IsOutputPrimitive<T>::value;
+};
+
+// std::deque
+// is an output sequence if and only if T is a primitive.
+template <class T, class Alloc>
+struct IsOutputSequence<std::deque<T, Alloc>> {
+  static constexpr bool value = IsOutputPrimitive<T>::value;
+};
+
+// std::forward_list
+// is an output sequence if and only if T is a primitive.
+template <class T, class Alloc>
+struct IsOutputSequence<std::forward_list<T, Alloc>> {
+  static constexpr bool value = IsOutputPrimitive<T>::value;
+};
+
+// std::array
+// is an output sequence if and only if T is a primitive.
+template <class T, std::size_t N>
+struct IsOutputSequence<std::array<T, N>> {
+  static constexpr bool value = IsOutputPrimitive<T>::value;
+};
+
+// std::map
+// is an output map if and only if both Key and T are primitives.
+template <class Key, class T, class Compare, class Alloc>
+struct IsOutputMap<std::map<Key, T, Compare, Alloc>> {
+  static constexpr bool value =
+      IsOutputPrimitive<Key>::value && IsOutputPrimitive<T>::value;
+};
+
+// std::unordered_map
+// is an output map if and only if both Key and T are primitives.
+template <class Key, class T, class Hash, class KeyEqual, class Alloc>
+struct IsOutputMap<std::unordered_map<Key, T, Hash, KeyEqual, Alloc>> {
+  static constexpr bool value =
+      IsOutputPrimitive<Key>::value && IsOutputPrimitive<T>::value;
+};
+
+// std::multimap
+// is an output sequence if and only if both Key and T are primitives.
+template <class Key, class T, class Compare, class Alloc>
+struct IsOutputSequence<std::multimap<Key, T, Compare, Alloc>> {
+  static constexpr bool value =
+      IsOutputPrimitive<Key>::value && IsOutputPrimitive<T>::value;
+};
+
+// std::unordered_multimap
+// is an output sequence if and only if both Key and T are primitives.
+template <class Key, class T, class Hash, class KeyEqual, class Alloc>
+struct IsOutputSequence<
+    std::unordered_multimap<Key, T, Hash, KeyEqual, Alloc>> {
+  static constexpr bool value =
+      IsOutputPrimitive<Key>::value && IsOutputPrimitive<T>::value;
+};
+
+// std::set
+// is an output sequence if and only if Key is a primitive.
+template <class Key, class Compare, class Alloc>
+struct IsOutputSequence<std::set<Key, Compare, Alloc>> {
+  static constexpr bool value = IsOutputPrimitive<Key>::value;
+};
+
+// std::unordered_set
+// is an output sequence if and only if Key is a primitive.
+template <class Key, class Hash, class KeyEqual, class Alloc>
+struct IsOutputSequence<std::unordered_set<Key, Hash, KeyEqual, Alloc>> {
+  static constexpr bool value = IsOutputPrimitive<Key>::value;
+};
+
+// std::pair
+// is a yaml scalar (because we define `<<`) provided that First and Second are
+// primitives.
+template <class First, class Second>
+typename std::enable_if<IsOutputPrimitive<First>::value &&
+                            IsOutputPrimitive<Second>::value,
+                        Emitter&>::type
+    operator<<(Emitter& emitter, const std::pair<First, Second>& pair) {
+  return emitter << BeginSeq << pair.first << pair.second << EndSeq;
+}
+
+// std::bitset
+// is a yaml scalar (because we define `<<`).
+template <std::size_t N>
+Emitter& operator<<(Emitter& emitter, const std::bitset<N>& bits) {
+  return emitter << bits.to_string();
+}
+
+// The emitter knows what to do with an output sequence (possibly recursive).
+template <class T>
+typename std::enable_if<IsOutputSequence<T>::value, Emitter&>::type operator<<(
+    Emitter& emitter, const T& sequence) {
   emitter << BeginSeq;
-  for (typename Seq::const_iterator it = seq.begin(); it != seq.end(); ++it)
-    emitter << *it;
+  for (const auto& item : sequence)
+    emitter << item;
   emitter << EndSeq;
   return emitter;
 }
 
-template <typename T>
-inline Emitter& operator<<(Emitter& emitter, const std::vector<T>& v) {
-  return EmitSeq(emitter, v);
-}
-
-template <typename T>
-inline Emitter& operator<<(Emitter& emitter, const std::list<T>& v) {
-  return EmitSeq(emitter, v);
-}
-
-template <typename T>
-inline Emitter& operator<<(Emitter& emitter, const std::set<T>& v) {
-  return EmitSeq(emitter, v);
-}
-
-template <typename K, typename V>
-inline Emitter& operator<<(Emitter& emitter, const std::map<K, V>& m) {
-  typedef typename std::map<K, V> map;
+// The emitter knows what to do with an output map (possibly recursive).
+template <class T>
+typename std::enable_if<IsOutputMap<T>::value, Emitter&>::type operator<<(
+    Emitter& emitter, const T& map) {
   emitter << BeginMap;
-  for (typename map::const_iterator it = m.begin(); it != m.end(); ++it)
-    emitter << Key << it->first << Value << it->second;
+  for (const auto& kv : map)
+    emitter << Key << kv.first << Value << kv.second;
   emitter << EndMap;
   return emitter;
 }
-}
+
+}  // namespace YAML
 
 #endif  // STLEMITTER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
